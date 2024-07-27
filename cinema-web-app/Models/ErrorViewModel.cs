ef299357b1488b34db8d4b2ae2c4ec0931efa155
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-27 15:59:58 +0300

    Initial commit

---
+++ cinema-web-app/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cinema_web_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
