f3406e48f01498ad00cee667b674f6aa630819ec
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-07-28 09:49:37 +0300

    - Create ReservationsController.cs - Create 5 basic screens for Reservations

---
+++ cinema-web-app/Controllers/ReservationsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cinema_web_app.Data;
+using cinema_web_app.Models;
+
+namespace cinema_web_app.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reservations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cinemas.ToListAsync());
+        }
+
+        // GET: Reservations/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cinema = await _context.Cinemas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+
+            return View(cinema);
+        }
+
+        // GET: Reservations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,City,ZipCode,Email,NoOfScreeningRooms")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                cinema.Id = Guid.NewGuid();
+                _context.Add(cinema);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cinema);
+        }
+
+        // GET: Reservations/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cinema = await _context.Cinemas.FindAsync(id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+            return View(cinema);
+        }
+
+        // POST: Reservations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,Address,City,ZipCode,Email,NoOfScreeningRooms")] Cinema cinema)
+        {
+            if (id != cinema.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cinema);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CinemaExists(cinema.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cinema);
+        }
+
+        // GET: Reservations/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cinema = await _context.Cinemas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+
+            return View(cinema);
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var cinema = await _context.Cinemas.FindAsync(id);
+            if (cinema != null)
+            {
+                _context.Cinemas.Remove(cinema);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CinemaExists(Guid id)
+        {
+            return _context.Cinemas.Any(e => e.Id == id);
+        }
+    }
+}
