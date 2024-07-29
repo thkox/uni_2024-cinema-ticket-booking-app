03e61d2644eb53f86724472937542cb7e8655e77
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-07-29 16:02:46 +0300

    Create ContentAppAdminsController.cs and Index.cshtml

---
+++ cinema-web-app/Controllers/ContentAppAdminsController.cs
@@ -0,0 +1,20 @@
+﻿using cinema_web_app.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cinema_web_app.Controllers
+{
+    public class ContentAppAdminsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContentAppAdminsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
