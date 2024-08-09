c3e583cc7b1a0daa7a53ca96050518c219e81975
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-08-09 14:29:52 +0300

    create the first layout of the ApplicationAdminsController.cs pages

---
+++ cinema-web-app/Models/EditUserViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace cinema_web_app.Models;
+
+public class EditUserViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
