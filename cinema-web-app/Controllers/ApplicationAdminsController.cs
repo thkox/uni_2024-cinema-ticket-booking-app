385728cd3732c41bb09cb1374cbc06b114732409
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-08-09 14:29:52 +0300

    create the first layout of the ApplicationAdminsController.cs pages

--- cinema-web-app/Controllers/ApplicationAdminsController.cs
+++ cinema-web-app/Controllers/ApplicationAdminsController.cs
@@ -1,21 +1,153 @@
-﻿using cinema_web_app.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using cinema_web_app.Models;
+using cinema_web_app.Data;
 
 namespace cinema_web_app.Controllers
 {
+    [Authorize(Roles = "ApplicationAdmin")]
     public class ApplicationAdminsController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole<Guid>> _roleManager;
         private readonly ApplicationDbContext _context;
-        
-        public ApplicationAdminsController(ApplicationDbContext context)
+
+        public ApplicationAdminsController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole<Guid>> roleManager, ApplicationDbContext context)
         {
+            _userManager = userManager;
+            _roleManager = roleManager;
             _context = context;
         }
-        
+
+        // GET: Admin/Index
         public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        // GET: Admin/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Admin/Create
+        public IActionResult Create()
         {
             return View();
         }
+
+        // POST: Admin/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FirstName,LastName,Email,Password,ConfirmPassword,Role")] CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.Role);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        // GET: Admin/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email
+            };
+            return View(model);
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,FirstName,LastName,Email")] EditUserViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(id.ToString());
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Email = model.Email;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user != null)
+            {
+                var result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
-
---
+++ cinema-web-app/Models/CreateUserViewModel.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace cinema_web_app.Models;
+
+public class CreateUserViewModel
+{
+    [Microsoft.Build.Framework.Required]
+    public string FirstName { get; set; }
+
+    [Microsoft.Build.Framework.Required]
+    public string LastName { get; set; }
+
+    [Microsoft.Build.Framework.Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Microsoft.Build.Framework.Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; }
+
+    [Microsoft.Build.Framework.Required]
+    public string Role { get; set; }
+}
